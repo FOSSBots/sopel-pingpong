67e5980957ffe3e962bdbd6b69867d60694a17c1
RhinosF1 <rhinosf1@gmail.com>
2021-04-16 15:30:16 +0100

    Release 1.0.5 (#15)
    * Add .deepsource.toml
    
    * Remove unnecessary use of comprehension
    
    * Update workflow.yml
    
    * Update requirements.txt
    
    * Update workflow.yml
    
    * Update workflow.yml
    
    * Update workflow.yml
    
    * Create .gitignore
    
    * Update workflow.yml
    
    * Update requirements.txt
    
    * Update setup.py
    
    Co-authored-by: DeepSource Bot <bot@deepsource.io>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: MacFan4000 <paulfkeffer@gmail.com>

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 with open('requirements.txt') as requirements_file:
-    requirements = [req for req in requirements_file.readlines()]
+    requirements = list(requirements_file.readlines())
 
 
 setup(
     name='sopel_plugins.pingpong',
-    version='1.0.4',
+    version='1.0.5',
     description='Pingpong plugin for Sopel',
     long_description=readme,
     long_description_content_type='text/markdown',  # This is important!
     author='MirahezeBot Contributors',
-    author_email='bots@miraheze.org',
+    author_email='staff@mirahezebots.org',
     url='https://github.com/MirahezeBots/sopel-pingpong',
     packages=find_packages('.'),
     include_package_data=True,
