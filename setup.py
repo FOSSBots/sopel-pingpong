cdf77068cec0f769fe63c74526edf625927ffe90
RhinosF1 <rhinosf1@gmail.com>
2021-04-16 14:51:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='sopel_plugins.pingpong',
-    version='1.0.4',
+    version='1.0.5',
     description='Pingpong plugin for Sopel',
     long_description=readme,
     long_description_content_type='text/markdown',  # This is important!
     author='MirahezeBot Contributors',
-    author_email='bots@miraheze.org',
+    author_email='staff@mirahezebots.org',
     url='https://github.com/MirahezeBots/sopel-pingpong',
     packages=find_packages('.'),
     include_package_data=True,
